f09da8cbc2c41711b671748a8be912e850a644f6
Dmitriy Azarov <oxozle@gmail.com>
2017-01-15 23:15:14 +0300

    Update interface builder

--- Source/OXPatternLock.swift
+++ Source/OXPatternLock.swift
@@ -11,28 +11,72 @@
 import Foundation
 import UIKit
 
+
+/// OXPatternLock Delegate
 public protocol OXPatternLockDelegate: class {
+    
+    /// Occurs when user ended to track pattern lock
+    ///
+    /// - Parameters:
+    ///   - patterLock: instance of locker
+    ///   - track: array of integers, which user tracked
     func didPatternInput(patterLock: OXPatternLock, track: [Int])
 }
 
+
+/// UI Element represent pattern lock
 @IBDesignable
 open class OXPatternLock: UIControl {
     public weak var delegate: OXPatternLockDelegate?
     
-    @IBInspectable
-    public var dot: UIImage?
     
+    /// Default state image
     @IBInspectable
-    public var dotSelected: UIImage?
+    public var dot: UIImage? {
+        didSet {
+            updateLayoutConfiguration()
+        }
+    }
     
-    @IBInspectable
-    public var lockSize: Int = 3
     
+    /// Tracked state image
     @IBInspectable
-    public var trackLineColor: UIColor = UIColor.white
+    public var dotSelected: UIImage?{
+        didSet {
+            updateLayoutConfiguration()
+        }
+    }
+
     
+    
+    /// Grid size
     @IBInspectable
-    public var trackLineThikness: CGFloat = 5
+    public var lockSize: Int = 3{
+        didSet {
+            updateLayoutConfiguration()
+        }
+    }
+
+    
+    
+    /// Color of track line
+    @IBInspectable
+    public var trackLineColor: UIColor = UIColor.white{
+        didSet {
+            updateLayoutConfiguration()
+        }
+    }
+
+    
+    
+    /// Thikness of track line
+    @IBInspectable
+    public var trackLineThikness: CGFloat = 5{
+        didSet {
+            updateLayoutConfiguration()
+        }
+    }
+
     
     private var lockTrack = [Int]()
     private var lockFrames = [CGRect]()
@@ -45,38 +89,24 @@ open class OXPatternLock: UIControl {
         guard let ctx = UIGraphicsGetCurrentContext() else {return}
         guard let dot = dot else {return}
         guard let dotSelected = dotSelected else {return}
-
-        if !lockTrack.isEmpty {
-            drawTrackPath(ctx: ctx)
-        }
-        
-        let spacing = (rect.width - (CGFloat(lockSize) * dot.size.width)) / CGFloat(lockSize + 1)
-        let iconSize = dot.size
-        let lockFramesSizeShouldBe = lockSize * lockSize
-        
-        for y in 0..<lockSize {
-            for x in 0..<lockSize {
-                let point = CGPoint(x: spacing + CGFloat(x) * (iconSize.width + spacing), y: spacing + CGFloat(y) * (iconSize.width + spacing))
-                let dotRect = CGRect(origin: point, size: iconSize)
-                if lockFrames.count < lockFramesSizeShouldBe {
-                    lockFrames.append(dotRect)
-                }
-                
-                if lockTrack.contains(x + y * lockSize) {
-                    dotSelected.draw(in: dotRect)
-                } else {
-                    dot.draw(in: dotRect)
-                }
-                
-            }
-        }
-        
         
         #if !TARGET_INTERFACE_BUILDER
             // this code will run in the app itself
         #else
-            // this code will execute only in IB
+            prepareTrackForInterfaceBuilder()
         #endif
+        
+        if !lockTrack.isEmpty {
+            drawTrackPath(ctx: ctx)
+        }
+        
+        for (index,dotRect) in lockFrames.enumerated() {
+            if lockTrack.contains(index) {
+                dotSelected.draw(in: dotRect)
+            } else {
+                dot.draw(in: dotRect)
+            }
+        }
     }
     
     private func drawTrackPath(ctx: CGContext) {
@@ -98,6 +128,27 @@ open class OXPatternLock: UIControl {
         ctx.setLineJoin(.round)
         ctx.addPath(path.cgPath)
         ctx.strokePath()
+    }
+    
+    //MARK: Layout
+    
+    private func updateLayoutConfiguration() {
+        guard let dot = dot else {return}
+        guard let _ = dotSelected else {return}
+        
+        let rect = self.frame
+        let spacing = (rect.width - (CGFloat(lockSize) * dot.size.width)) / CGFloat(lockSize + 1)
+        let iconSize = dot.size
+        lockFrames.removeAll()
+        
+        
+        for y in 0..<lockSize {
+            for x in 0..<lockSize {
+                let point = CGPoint(x: spacing + CGFloat(x) * (iconSize.width + spacing), y: spacing + CGFloat(y) * (iconSize.width + spacing))
+                let dotRect = CGRect(origin: point, size: iconSize)
+                lockFrames.append(dotRect)
+            }
+        }
     }
     
     
@@ -134,13 +185,22 @@ open class OXPatternLock: UIControl {
             if rect.contains(point) {
                 if !lockTrack.contains(index) {
                     lockTrack.append(index)
-                    print("current lock track: \(lockTrack)")
                 }
                 return true
             }
         }
         
         return false
+    }
+    
+    //MARK: IBDesignable
+    private func prepareTrackForInterfaceBuilder() {
+        lockTrack.removeAll()
+        lockTrack.append(0)
+        lockTrack.append(1)
+        lockTrack.append(lockSize)
+        lockTrack.append(lockSize + 1)
+        lockTrack.append(lockSize * 2 + 1)
     }
 }
 
